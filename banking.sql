332c18de0024d2854914996d820839f89ad51dab
Prathibha-21 <bolliniprathibha9642@gmail.com>
2023-07-26 03:01:59 +0000

    Backup Commit

--- banking.sql
+++ banking.sql
@@ -1,6 +1,27 @@
 --write a sql query to find the highest amount of the debited from the bank each year
-select max(withdrawal_amt)
-from bank_transaction;
+select extract(year from "date") as Year ,
+max(cast(regexp_replace(withdrawal_amt,'[^0-9.]','')as number)) as HighestDebitedAmount
+from bank_transaction
+where WITHDRAWAL_AMT is not null
+group by extract(year from "date") order by extract(year from "date");
+
+
 --write a sql query to find the lowest of the debited from the bank each year
-select min(withdrawal_amt)
-from bank_transaction;
+select extract(year from "date") as Year ,
+MIN(cast(regexp_replace(withdrawal_amt,'[^0-9.]','')as number)) as lowestestDebitedAmount
+from bank_transaction
+where WITHDRAWAL_AMT is not null
+group by extract(year from "date") order by extract(year from "date");
+
+
+--write a sql query to find the 5th highest withdrawal amount at each year?
+select year, FifthHighestAmount
+from(
+    select extract(year from "date") as year,
+    cast(REGEXP_REPLACE(withdrawal_amt, '[0-9.]','') as number) as FifthHighestAmount,
+    dense_rank() over(partition by extract(year from "date") order BY
+    cast(REGEXP_REPLACE(withdrawal_amt, '[0-9.]','') as number) desc) as R
+    from BANK_TRANSACTION
+    where cast(REGEXP_REPLACE(withdrawal_amt, '[0-9.]','') as number) is not null)
+    where R=5;
+)
